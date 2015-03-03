a0db5af62af1a77517f658e98d312ab95180c0f2
William Griffin <williamgriffin989@gmail.com>
2015-03-02 22:45:21 -0500

    finished removing turbolinks, adding angular, underscore

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jqueryi
 //= require jquery_ujs
-//= require turbolinks
+
 //= require_tree .
+
+//= require angular
+//= require underscore
